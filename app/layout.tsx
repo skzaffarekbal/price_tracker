51de5f16b7adc2266a7de8708ec87d98dd2b2e4b
Sk Zaffar Ekbal <s.zaffarekbal@gmail.com>
2024-05-19 03:59:16 +0530

    Add Vercel SpeedInsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter, Space_Grotesk } from 'next/font/google';
 import './globals.css';
 import Navbar from '@/components/Navbar';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ['latin'] });
 const spaceGrotesk = Space_Grotesk({
@@ -25,6 +26,7 @@ export default function RootLayout({
         <main className='max-w-10xl mx-auto'>
           <Navbar />
           {children}
+          <SpeedInsights />
         </main>
       </body>
     </html>
